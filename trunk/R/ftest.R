56cdfe4786594a5b14bba113c281d756da183dfc
henningsena <henningsena@591db2bd-e0a0-4d22-8c19-2192dbb7da31>
2006-02-17 15:49:58 +0000

    revised F-test calculation, documentation and print method (it uses now the residual covariance matrix that was used for the estimation)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/systemfit@167 591db2bd-e0a0-4d22-8c19-2192dbb7da31

--- trunk/R/ftest.R
+++ trunk/R/ftest.R
@@ -4,9 +4,13 @@ ftest.systemfit <- function( object, R.restr,
    coef <- coef( object )
    vcov <- vcov( object )
    resid <- unlist( residuals( object ) )
-   rcov  <- object$rcov
    nEq   <- object$g
    nObsEq <- object$n / nEq
+   if( is.null( object$rcovest ) ) {
+      rcov <- diag( nEq )
+   } else {
+      rcov <- object$rcovest
+   }
 
    result <- list()
 
@@ -20,6 +24,7 @@ ftest.systemfit <- function( object, R.restr,
    denominator <- t( resid ) %*%
       ( solve( rcov ) %x% diag( nObsEq ) ) %*%
       resid
+   #print( denominator )
 
    result$statistic <- ( numerator / result$nRestr ) /
       ( denominator / result$dfSys )
@@ -34,6 +39,8 @@ print.ftest.systemfit <- function( x, digits = 4, ... ){
    cat( "\n", "F-test for linear parameter restrictions",
       " in equation systems\n", sep = "" )
    cat( "F-statistic:", formatC( x$statistic, digits = digits ), "\n" )
-   cat( "p-value:", formatC( x$p.value, digits = digits ), "\n" )
+   cat( "degrees of freedom of the numerator:", x$nRestr, "\n" )
+   cat( "degrees of freedom of the denominator:", x$dfSys, "\n" )
+   cat( "p-value:", formatC( x$p.value, digits = digits ), "\n\n" )
    invisible( x )
 }
