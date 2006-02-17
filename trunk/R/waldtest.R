c590b58c83bf156caed87dac44f225b875d63682
henningsena <henningsena@591db2bd-e0a0-4d22-8c19-2192dbb7da31>
2006-02-17 15:47:17 +0000

    revised Wald-test documentation and print method
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/systemfit@166 591db2bd-e0a0-4d22-8c19-2192dbb7da31

--- trunk/R/waldtest.R
+++ trunk/R/waldtest.R
@@ -22,6 +22,7 @@ print.waldtest.systemfit <- function( x, digits = 4, ... ){
    cat( "\n", "Wald-test for linear parameter restrictions",
       " in equation systems\n", sep = "" )
    cat( "Wald-statistic:", formatC( x$statistic, digits = digits ), "\n" )
-   cat( "p-value:", formatC( x$p.value, digits = digits ), "\n" )
+   cat( "degrees of freedom:", x$nRestr, "\n" )
+   cat( "p-value:", formatC( x$p.value, digits = digits ), "\n\n" )
    invisible( x )
 }
