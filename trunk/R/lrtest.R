a7c2fc61d8b34167eda3dd146f7f474e66b95d3f
henningsena <henningsena@591db2bd-e0a0-4d22-8c19-2192dbb7da31>
2006-02-17 15:52:29 +0000

    considerably revised LR-test documentation, added print method, renamed objects returned by lrtest.systemfit
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/systemfit@168 591db2bd-e0a0-4d22-8c19-2192dbb7da31

--- trunk/R/lrtest.R
+++ trunk/R/lrtest.R
@@ -4,20 +4,31 @@ lrtest.systemfit <- function( resultc, resultu ) {
   if( resultc$method %in% c( "SUR", "WSUR" ) &
       resultu$method %in% c( "SUR", "WSUR" ) ) {
     n   <- resultu$eq[[1]]$n
-    lrtest$df  <- resultu$ki - resultc$ki
+    lrtest$nRestr  <- resultu$ki - resultc$ki
     if(resultc$rcovformula != resultu$rcovformula) {
       stop( paste( "both estimations must use the same formula to calculate",
                    "the residual covariance matrix!" ) )
     }
     if(resultc$rcovformula == 0) {
-      lrtest$lr  <- n * ( log( resultc$drcov ) - log( resultu$drcov ) )
+      lrtest$statistic  <- n * ( log( resultc$drcov ) - log( resultu$drcov ) )
     } else {
       residc <- array(resultc$resids,c(n,resultc$g))
       residu <- array(resultu$resids,c(n,resultu$g))
-      lrtest$lr <- n * ( log( det( (t(residc) %*% residc)) ) -
+      lrtest$statistic <- n * ( log( det( (t(residc) %*% residc)) ) -
                          log( det( (t(residu) %*% residu))))
     }
-    lrtest$p <- 1-pchisq( lrtest$lr, lrtest$df )
+    lrtest$p.value <- 1 - pchisq( lrtest$statistic, lrtest$nRestr )
   }
-  lrtest
+  class( lrtest ) <- "lrtest.systemfit"
+  return( lrtest )
 }
+
+print.lrtest.systemfit <- function( x, digits = 4, ... ){
+   cat( "\n", "Likelihood-Ratio-test for parameter restrictions",
+      " in equation systems\n", sep = "" )
+   cat( "LR-statistic:", formatC( x$statistic, digits = digits ), "\n" )
+   cat( "degrees of freedom:", x$nRestr, "\n" )
+   cat( "p-value:", formatC( x$p.value, digits = digits ), "\n\n" )
+   invisible( x )
+}
+
