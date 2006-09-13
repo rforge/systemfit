0cd885577a08d17ccc94497b9ead37f9bfbde7bd
henningsena <henningsena@591db2bd-e0a0-4d22-8c19-2192dbb7da31>
2006-09-13 19:32:15 +0000

    predict.systemfit() calls now predict.systemfit.equation() for each equation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/systemfit@280 591db2bd-e0a0-4d22-8c19-2192dbb7da31

--- trunk/R/predict.systemfit.R
+++ trunk/R/predict.systemfit.R
@@ -9,124 +9,22 @@ predict.systemfit <- function( object, newdata=object$data,
          # TRUE if there are restrictions imposed
    }
 
-   # calculate residual covariance matrix with centered residuals and
-   # correction for degrees of freedom
-   rcov <- .calcRCov( object$resids, methodRCov = "geomean",
-      nObsEq = object$nObsEq, nCoefEq = object$nExogLiEq,
-      centered = TRUE, solvetol = object$control$solvetol )
-
-   predicted <- data.frame( obs=seq( nrow( newdata ) ) )
-   colnames( predicted ) <- as.character( 1:ncol( predicted ) )
-
-   # get equations
-   eqns <- list()
    for(i in 1:object$nEq )  {
-      eqns[[i]] <- formula( object$eq[[i]]$terms )
-   }
-
-   # prepare data
-   preparedData <- .prepareData.systemfit( data = newdata, eqns = eqns )
-   # number of observations in each equation
-   nObsEq  <- preparedData$nObsEq
-   # list for matrices of regressors in each equation
-   xMatEq <- preparedData$xMatEq
-   # stacked matrices of all regressors
-   xMatAll <- preparedData$xMatAll
-
-   yVecAll <- xMatAll %*% object$coef
-   if( object$method %in% c( "SUR", "WSUR", "3SLS", "W3SLS" ) ){
-      if( se.fit | interval == "confidence" ) {
-         ycovc <- xMatAll %*% object$bcov %*% t(xMatAll)
-      }
-      if( se.pred | interval == "prediction" ) {
-         ycovp <- xMatAll %*% object$bcov %*% t(xMatAll) + rcov %x% diag(1,nObsEq[1],nObsEq[1])
+      predicted.i <- predict( object$eq[[ i ]], newdata = newdata,
+         se.fit = se.fit, se.pred = se.pred, interval = interval,
+         level = level, probDfSys = probDfSys )
+      names( predicted.i ) <- paste( "eq", i, ".",
+         names( predicted.i ), sep = "" )
+      if( i == 1 ) {
+         predicted <- predicted.i
+      } else {
+         predicted <- cbind( predicted, predicted.i )
       }
    }
-   for(i in 1:object$nEq) {
-      # fitted values
-      Yi <- yVecAll[(1+sum(nObsEq[1:i])-nObsEq[i]):sum(nObsEq[1:i]),]
-      predicted <- cbind( predicted, Yi )
-      names( predicted )[ length( predicted ) ] <- paste( "eq", as.character(i),
-                                                          ".pred", sep="" )
-      # calculate variance covariance matrices
-      if( se.fit | interval == "confidence" ) {
-         if( object$method %in% c( "SUR", "WSUR", "3SLS", "W3SLS" ) ){
-            ycovci <- ycovc[ ( 1 + sum( nObsEq[1:i] ) - nObsEq[i] ) : sum( nObsEq[1:i] ),
-                             ( 1 + sum( nObsEq[1:i] ) - nObsEq[i] ) : sum( nObsEq[1:i] ) ]
-         } else {
-            ycovci <- xMatEq[[i]] %*% object$eq[[i]]$bcov %*% t(xMatEq[[i]])
-         }
-      }
-      if( se.pred | interval == "prediction" ) {
-         if( object$method %in% c( "SUR", "WSUR", "3SLS", "W3SLS" ) ){
-            ycovpi <- ycovp[ ( 1 + sum( nObsEq[1:i] ) - nObsEq[i] ) : sum( nObsEq[1:i] ),
-                            ( 1 + sum( nObsEq[1:i] ) - nObsEq[i] ) : sum( nObsEq[1:i] ) ]
-         } else {
-            ycovpi <- xMatEq[[i]] %*% object$eq[[i]]$bcov %*% t(xMatEq[[i]]) +
-                                 object$eq[[i]]$sigma^2
-         }
-      }
-      # standard errors of fitted values
-      if( se.fit ) {
-         if(nrow(newdata)==1) {
-            predicted <- cbind( predicted, sqrt( ycovci ) )
-         } else {
-            predicted <- cbind( predicted, sqrt( diag( ycovci ) ) )
-         }
-         names( predicted )[ length( predicted ) ] <-
-            paste( "eq", as.character(i), ".se.fit", sep="" )
-      }
-      # standard errors of prediction
-      if( se.pred ) {
-         if(nrow(newdata)==1) {
-            predicted <- cbind( predicted, sqrt( ycovpi ) )
-         } else {
-            predicted <- cbind( predicted, sqrt( diag( ycovpi ) ) )
-         }
-         names( predicted )[ length( predicted ) ] <-
-            paste( "eq", as.character(i), ".se.pred", sep="" )
-      }
+   names( predicted ) <- sub( "([0-9])\.fit", "\\1.pred",
+         names( predicted ) )
 
-      # confidence intervals
-      if( interval == "confidence" ) {
-         if( probDfSys ) {
-            tval   <- qt( 1 - ( 1- level )/2, object$df )
-         } else {
-            tval   <- qt( 1 - ( 1- level )/2, object$eq[[i]]$df )
-         }
-         if( nrow(newdata)==1 ) {
-            seci    <- sqrt( ycovci )
-         } else {
-            seci    <- sqrt( diag( ycovci ) )
-         }
-         predicted <- cbind( predicted, Yi - ( tval * seci ) )
-         names( predicted )[ length( predicted ) ] <-
-            paste( "eq", as.character(i), ".lwr", sep="" )
-         predicted <- cbind( predicted, Yi + ( tval * seci ) )
-         names( predicted )[ length( predicted ) ] <-
-            paste( "eq", as.character(i), ".upr", sep="" )
-      }
-      # prediction intervals
-      if( interval == "prediction" ) {
-         if( probDfSys ) {
-            tval   <- qt( 1 - ( 1- level )/2, object$df )
-         } else {
-            tval   <- qt( 1 - ( 1- level )/2, object$eq[[i]]$df )
-         }
-         if(nrow(newdata)==1) {
-            sepi <- sqrt( ycovpi )
-         } else {
-            sepi <- sqrt( diag( ycovpi ) )
-         }
-         predicted <- cbind( predicted, Yi - ( tval * sepi ) )
-         names( predicted )[ length( predicted ) ] <-
-            paste( "eq", as.character(i), ".lwr", sep="" )
-         predicted <- cbind( predicted, Yi + ( tval * sepi ) )
-         names( predicted )[ length( predicted ) ] <-
-            paste( "eq", as.character(i), ".upr", sep="" )
-      }
-   }
-   predicted[ 2: length( predicted ) ]
+   return( predicted )
 }
 
 ## calculate predicted values, its standard errors and the prediction intervals
