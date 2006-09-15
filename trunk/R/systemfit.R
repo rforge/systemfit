bc59767627686c458439678f2f56a7b9fec05dbe
henningsena <henningsena@591db2bd-e0a0-4d22-8c19-2192dbb7da31>
2006-09-15 05:01:56 +0000

    moved print.systemfit() and print.systemfit.equation() into a separate file: print.systemfit.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/systemfit@288 591db2bd-e0a0-4d22-8c19-2192dbb7da31

--- trunk/R/systemfit.R
+++ trunk/R/systemfit.R
@@ -591,60 +591,6 @@ systemfit <- function(  eqns,
 }
 
 
-## print a few results of the whole system
-print.systemfit <- function( x, digits=6,... ) {
-
-   save.digits <- unlist(options(digits=digits))
-   on.exit(options(digits=save.digits))
-
-   cat("\n")
-   cat("systemfit results \n")
-   cat("method: ")
-   if(!is.null(x$iter)) if(x$iter>1) cat("iterated ")
-   cat( paste( x$method, "\n\n"))
-   if(!is.null(x$iter)) {
-      if(x$iter>1) {
-         if(x$iter<x$control$maxiter) {
-            cat( paste( "convergence achieved after",x$iter,"iterations\n\n" ) )
-         } else {
-            cat( paste( "warning: convergence not achieved after", x$iter,
-                        "iterations\n\n" ) )
-         }
-      }
-   }
-   cat( "Coefficients:\n" )
-   print( x$coef )
-   invisible( x )
-}
-
-
-## print a few results for a single equation
-print.systemfit.equation <- function( x, digits=6, ... ) {
-
-   save.digits <- unlist(options(digits=digits))
-   on.exit(options(digits=save.digits))
-
-   cat("\n")
-   if( is.null( x$eqnlabel ) ) {
-      cat( x$method, " estimates for equation ", x$i, "\n", sep = "" )
-   } else {
-      cat( x$method, " estimates for '", x$eqnlabel,
-            "' (equation ", x$i, ")\n", sep = "" )
-   }
-
-   cat("Model Formula: ")
-   print( formula( x$terms ) )
-   if(!is.null(x$inst)) {
-      cat("Instruments: ")
-      print(x$inst)
-   }
-
-   cat("\nCoefficients:")
-   print( x$coef )
-   invisible( x )
-}
-
-
 ## this function returns a vector of the
 ## cross-equation corrlations between eq i and eq j
 ## from the results set for equation ij
