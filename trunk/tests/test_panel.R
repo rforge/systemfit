a53f21a8539e7ddcbfd62936396a42944f6a260b
henningsena <henningsena@591db2bd-e0a0-4d22-8c19-2192dbb7da31>
2007-06-21 10:13:17 +0000

    added tests for panel-like SUR models: coef( ), coef( , transformed = TRUE ), coef( summary( ) ), coef( summary( ), transformed = TRUE ), vcov( ), vcov( , transformed = TRUE ), residuals( ), confint( ), fitted( ), logLik( ), model.frame( ), model.matrix( )
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/systemfit@392 591db2bd-e0a0-4d22-8c19-2192dbb7da31

--- trunk/tests/test_panel.R
+++ trunk/tests/test_panel.R
@@ -11,12 +11,30 @@ theilOls <- systemfit( formulaGrunfeld, "OLS",
    data = GrunfeldTheil )
 print( theilOls )
 summary( theilOls )
+coef( theilOls )
+coef( summary(theilOls ) )
+vcov( theilOls )
+residuals( theilOls )
+confint( theilOls )
+fitted(theilOls  )
+logLik( theilOls )
+model.frame( theilOls )
+model.matrix( theilOls )
 
 # SUR
 theilSur <- systemfit( formulaGrunfeld, "SUR",
    data = GrunfeldTheil, methodRCov = "noDfCor" )
 print( theilSur )
 summary( theilSur )
+coef( theilSur )
+coef( summary( theilSur ) )
+vcov( theilSur )
+residuals( theilSur )
+confint( theilSur )
+fitted( theilSur )
+logLik( theilSur )
+model.frame( theilSur )
+model.matrix( theilSur )
 
 
 ## Repeating the OLS and SUR estimations in Greene (2003, pp. 351)
@@ -30,6 +48,15 @@ greeneOls <- systemfit( formulaGrunfeld, "OLS",
 print( greeneOls )
 summary( greeneOls )
 sapply( greeneOls$eq, function(x){return(x$ssr/20)} ) # sigma^2
+coef( greeneOls )
+coef( summary( greeneOls ) )
+vcov( greeneOls )
+residuals( greeneOls )
+confint(greeneOls  )
+fitted( greeneOls )
+logLik( greeneOls )
+model.frame( greeneOls )
+model.matrix( greeneOls )
 
 # OLS Pooled
 greeneOlsPooled <- systemfit( formulaGrunfeld, "OLS",
@@ -37,15 +64,48 @@ greeneOlsPooled <- systemfit( formulaGrunfeld, "OLS",
 print( greeneOlsPooled )
 summary( greeneOlsPooled )
 sum( sapply( greeneOlsPooled$eq, function(x){return(x$ssr)}) )/97 # sigma^2
+coef( greeneOlsPooled )
+coef( greeneOlsPooled, transformed = TRUE )
+coef( summary( greeneOlsPooled ) )
+coef( summary( greeneOlsPooled ), transformed = TRUE )
+vcov( greeneOlsPooled )
+vcov( greeneOlsPooled, transformed = TRUE )
+residuals( greeneOlsPooled )
+confint( greeneOlsPooled )
+fitted( greeneOlsPooled )
+logLik( greeneOlsPooled )
+model.frame( greeneOlsPooled )
+model.matrix( greeneOlsPooled )
 
 # SUR
 greeneSur <- systemfit( formulaGrunfeld, "SUR",
    data = GrunfeldGreene, methodRCov = "noDfCor" )
 print( greeneSur )
 summary( greeneSur )
+coef( greeneSur )
+coef( summary( greeneSur ) )
+vcov( greeneSur )
+residuals( greeneSur )
+confint( greeneSur )
+fitted( greeneSur )
+logLik( greeneSur )
+model.frame( greeneSur )
+model.matrix( greeneSur )
 
 # SUR Pooled
 greeneSurPooled <- systemfit( formulaGrunfeld, "WSUR",
    data = GrunfeldGreene, pooled = TRUE, methodRCov = "noDfCor" )
 print( greeneSurPooled )
 summary( greeneSurPooled )
+coef( greeneSurPooled )
+coef( greeneSurPooled, transformed = TRUE )
+coef( summary( greeneSurPooled ) )
+coef( summary( greeneSurPooled ), transformed = TRUE )
+vcov( greeneSurPooled )
+vcov( greeneSurPooled, transformed = TRUE )
+residuals( greeneSurPooled )
+confint( greeneSurPooled )
+fitted( greeneSurPooled )
+logLik( greeneSurPooled )
+model.frame( greeneSurPooled )
+model.matrix( greeneSurPooled )
