e745b29ff46423a712dde3bb5c63ccb493fadd9f
henningsena <henningsena@591db2bd-e0a0-4d22-8c19-2192dbb7da31>
2007-07-03 17:18:40 +0000

    added appendix (section) that shows how to estimate systems of equations with "sem"
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/systemfit@787 591db2bd-e0a0-4d22-8c19-2192dbb7da31

--- paper/trunk/runSweave.sh
+++ paper/trunk/runSweave.sh
@@ -2,3 +2,5 @@
 echo "Sweave(\"systemfit_usage.Snw\")" | R --no-save --no-restore
 
 echo "Sweave(\"systemfit_reliability.Snw\")" | R --no-save --no-restore
+
+echo "Sweave(\"systemfit_sem.Snw\")" | R --no-save --no-restore
