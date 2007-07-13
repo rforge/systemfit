bfb652fb7fbf45570fca5182af252d07709da872
henningsena <henningsena@591db2bd-e0a0-4d22-8c19-2192dbb7da31>
2007-07-13 06:40:56 +0000

    section "code" is generated with "Sweave" now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/systemfit@804 591db2bd-e0a0-4d22-8c19-2192dbb7da31

--- paper/trunk/runSweave.sh
+++ paper/trunk/runSweave.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo "Sweave(\"systemfit_code.Snw\")" | R --no-save --no-restore
+
 echo "Sweave(\"systemfit_usage.Snw\")" | R --no-save --no-restore
 
 echo "Sweave(\"systemfit_reliability.Snw\")" | R --no-save --no-restore
