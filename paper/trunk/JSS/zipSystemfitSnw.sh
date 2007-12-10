e21ba9f07625e71521fd005097439a1b70aac044
henningsena <henningsena@591db2bd-e0a0-4d22-8c19-2192dbb7da31>
2007-12-10 22:41:45 +0000

    added shell scripts to create zip archives of all source files (tex, Snw, bib)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/systemfit@905 591db2bd-e0a0-4d22-8c19-2192dbb7da31

---
+++ paper/trunk/JSS/zipSystemfitSnw.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+zip -j systemfit_Snw.zip \
+   ../runSweave.sh \
+   ../systemfit.tex \
+   ../systemfit_abstract.tex \
+   ../systemfit_intro.tex \
+   ../systemfit_statistics.tex \
+   ../systemfit_estimation.tex \
+   ../systemfit_restrictions.tex \
+   ../systemfit_other.tex \
+   ../systemfit_code.Snw \
+   ../systemfit_usage.Snw \
+   ../systemfit_reliability.Snw \
+   ../systemfit_summary.tex \
+   ../systemfit_compare_lm.tex \
+   ../systemfit_timings.Snw \
+   ../systemfit_sem.Snw \
+   ../systemfit.bib \
+   ~/texmf/bibtex/bst/jss.bst
---
+++ paper/trunk/JSS/zipSystemfitTex.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+zip -j systemfit_Tex.zip \
+   ../systemfit.tex \
+   ../systemfit_abstract.tex \
+   ../systemfit_intro.tex \
+   ../systemfit_statistics.tex \
+   ../systemfit_estimation.tex \
+   ../systemfit_restrictions.tex \
+   ../systemfit_other.tex \
+   ../systemfit_code.tex \
+   ../systemfit_usage.tex \
+   ../systemfit_reliability.tex \
+   ../systemfit_summary.tex \
+   ../systemfit_compare_lm.tex \
+   ../systemfit_timings.tex \
+   ../systemfit_sem.tex \
+   ../systemfit.bib \
+   ~/texmf/bibtex/bst/jss.bst
