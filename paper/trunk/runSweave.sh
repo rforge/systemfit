7e24b851f0c60e64109247422888e3b35dcc11a6
henningsena <henningsena@591db2bd-e0a0-4d22-8c19-2192dbb7da31>
2007-12-27 21:49:23 +0000

    runSweave.sh now calls Stangle to create 4 .R files that include the R code of 4 sections
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/systemfit@937 591db2bd-e0a0-4d22-8c19-2192dbb7da31

--- paper/trunk/runSweave.sh
+++ paper/trunk/runSweave.sh
@@ -2,9 +2,17 @@
 echo "Sweave(\"systemfit_code.Snw\")" | R --no-save --no-restore
 
 echo "Sweave(\"systemfit_usage.Snw\")" | R --no-save --no-restore
+echo "Stangle(\"systemfit_usage.Snw\", annotate=FALSE, split=TRUE,
+   prefix=FALSE)" | R --no-save --no-restore
 
 echo "Sweave(\"systemfit_reliability.Snw\")" | R --no-save --no-restore
+echo "Stangle(\"systemfit_reliability.Snw\", annotate=FALSE, split=TRUE,
+   prefix=FALSE)" | R --no-save --no-restore
 
 echo "Sweave(\"systemfit_timings.Snw\")" | R --no-save --no-restore
+echo "Stangle(\"systemfit_timings.Snw\", annotate=FALSE, split=TRUE,
+   prefix=FALSE)" | R --no-save --no-restore
 
 echo "Sweave(\"systemfit_sem.Snw\")" | R --no-save --no-restore
+echo "Stangle(\"systemfit_sem.Snw\", annotate=FALSE, split=TRUE,
+   prefix=FALSE)" | R --no-save --no-restore
