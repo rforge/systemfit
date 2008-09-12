c199d675d57fbbfcf246a7d92f3ccb86f07b7085
henningsena <henningsena@591db2bd-e0a0-4d22-8c19-2192dbb7da31>
2008-09-12 10:45:07 +0000

    moved section "Replication of textbook results" (systemfit_reliability.Snw) into master file (systemfit.Rnw)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/systemfit@956 591db2bd-e0a0-4d22-8c19-2192dbb7da31

--- paper/trunk/runSweave.sh
+++ paper/trunk/runSweave.sh
@@ -4,10 +4,6 @@ echo "Sweave(\"systemfit.Rnw\")" | LC_ALL="C" R --no-save --no-restore
 echo "Stangle(\"systemfit.Rnw\", annotate=FALSE, split=TRUE,
    prefix=FALSE)" | LC_ALL="C" R --no-save --no-restore
 
-echo "Sweave(\"systemfit_reliability.Snw\")" | LC_ALL="C" R --no-save --no-restore
-echo "Stangle(\"systemfit_reliability.Snw\", annotate=FALSE, split=TRUE,
-   prefix=FALSE)" | LC_ALL="C" R --no-save --no-restore
-
 echo "Sweave(\"systemfit_timings.Snw\")" | R --no-save --no-restore
 echo "Stangle(\"systemfit_timings.Snw\", annotate=FALSE, split=TRUE,
    prefix=FALSE)" | LC_ALL="C" R --no-save --no-restore
