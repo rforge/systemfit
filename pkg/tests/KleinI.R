95d808803b01d8f5be71cda4cee0b4595f71e9e3
arne <arne@591db2bd-e0a0-4d22-8c19-2192dbb7da31>
2009-06-10 19:20:32 +0000

    the tests using Klein's (1950) Model I of the US economy are implemented using a for() loop over different estimation methods now (instead of copy-and-paste)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/systemfit@1008 591db2bd-e0a0-4d22-8c19-2192dbb7da31

--- pkg/tests/KleinI.R
+++ pkg/tests/KleinI.R
@@ -8,56 +8,46 @@ inst <- ~ govExp + taxes + govWage + trend + capitalLag + corpProfLag + gnpLag
 system <- list( Consumption = eqConsump, Investment = eqInvest,
    PrivateWages = eqPrivWage )
 
-# OLS
-kleinOls <- systemfit( system, data = KleinI )
-summary( kleinOls )
-residuals( kleinOls )
-fitted( kleinOls )
-predict( kleinOls, se.fit = TRUE, interval = "prediction",
-   useDfSys = TRUE )
-model.frame( kleinOls )
-model.matrix( kleinOls )
+for( methodNo in 1:5 ) {
+   method <- c( "OLS", "2SLS", "SUR", "3SLS", "3SLS" )[ methodNo ]
+   maxit <- ifelse( methodNo == 5, 500, 1 )
 
-# 2SLS
-klein2sls <- systemfit( system, "2SLS", inst = inst, data = KleinI,
-   methodResidCov = "noDfCor" )
-summary( klein2sls )
-residuals( klein2sls )
-fitted( klein2sls )
-predict( klein2sls, se.fit = TRUE, interval = "confidence",
-   useDfSys = FALSE )
-model.frame( klein2sls )
-all.equal( model.matrix( kleinOls ), model.matrix( klein2sls ) )
-
-# SUR
-kleinSur <- systemfit( system, "SUR", data = KleinI,
-   methodResidCov = "noDfCor" )
-summary( kleinSur )
-residuals( kleinSur )
-fitted( kleinSur )
-predict( kleinSur, se.fit = TRUE, interval = "confidence",
-   useDfSys = TRUE )
-all.equal( model.frame( kleinOls ), model.frame( kleinSur ) )
-all.equal( model.matrix( kleinOls ), model.matrix( kleinSur ) )
-
-# 3SLS
-klein3sls <- systemfit( system, "3SLS", inst = inst, data = KleinI,
-   methodResidCov = "noDfCor" )
-summary( klein3sls )
-residuals( klein3sls )
-fitted( klein3sls )
-predict( klein3sls, se.fit = TRUE, interval = "prediction",
-   useDfSys = FALSE )
-all.equal( model.frame( klein2sls ), model.frame( klein3sls ) )
-all.equal( model.matrix( kleinOls ), model.matrix( klein3sls ) )
-
-# I3SLS
-kleinI3sls <- systemfit( system, "3SLS", inst = inst, data = KleinI,
-   methodResidCov = "noDfCor", maxit = 500 )
-summary( kleinI3sls )
-residuals( kleinI3sls )
-fitted( kleinI3sls )
-predict( kleinI3sls, se.fit = TRUE, interval = "confidence",
-   useDfSys = TRUE )
-all.equal( model.frame( klein2sls ), model.frame( kleinI3sls ) )
-all.equal( model.matrix( kleinOls ), model.matrix( kleinI3sls ) )
+   cat( "> \n> # ", ifelse( maxit == 1, "", "I" ), method, "\n", sep = "" )
+   if( method %in% c( "OLS", "WLS", "SUR" ) ) {
+      kleinModel <- systemfit( system, method = method, data = KleinI,
+         methodResidCov = ifelse( method == "OLS", "geomean", "noDfCor" ),
+         maxit = maxit )
+   } else {
+      kleinModel <- systemfit( system, method = method, data = KleinI,
+         inst = inst, methodResidCov = "noDfCor", maxit = maxit )
+   }
+   cat( "> summary\n" )
+   print( summary( kleinModel ) )
+   cat( "> residuals\n" )
+   print( residuals( kleinModel ) )
+   cat( "> fitted\n" )
+   print( fitted( kleinModel ) )
+   cat( "> predict\n" )
+   print( predict( kleinModel, se.fit = TRUE,
+      interval = ifelse( methodNo %in% c( 1, 4 ), "prediction",  "confidence" ),
+      useDfSys = methodNo %in% c( 1, 3, 5 ) ) )
+   cat( "> model.frame\n" )
+   if( methodNo == 1 ) {
+      mfOls <- model.frame( kleinModel )
+      print( mfOls )
+   } else if( methodNo == 2 ) {
+      mf2sls <- model.frame( kleinModel )
+      print( mf2sls )
+   } else if( methodNo == 3 ) {
+      print( all.equal( mfOls, model.frame( kleinModel ) ) )
+   } else {
+      print( all.equal( mf2sls, model.frame( kleinModel ) ) )
+   }
+   cat( "> model.matrix\n" )
+   if( methodNo == 1 ) {
+      mmOls <- model.matrix( kleinModel )
+      print( mmOls )
+   } else {
+      print( all.equal( mmOls, model.matrix( kleinModel ) ) )
+   }
+}
