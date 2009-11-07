2c03c504ff827a5848b5c8e76d398431100b3fc0
arne <arne@591db2bd-e0a0-4d22-8c19-2192dbb7da31>
2009-11-07 16:19:58 +0000

    the logLik method should calculate the log-likelihood value for unbalanced systems correctly now (it uses the conditional density function for the unbalanced observations assuming that the errors of not-included observations are zero)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/systemfit@1045 591db2bd-e0a0-4d22-8c19-2192dbb7da31

--- pkg/R/logLik.R
+++ pkg/R/logLik.R
@@ -2,11 +2,31 @@ logLik.systemfit <- function( object, ... ){
 
    resid <- residuals( object )
    residCov <- .calcResidCov( resid, "noDfCor" )
+   residCovInv <- solve( residCov )
+   resid <- as.matrix( resid )
    nEq <- ncol( resid )
-   nObsPerEq <- sum( !is.na( resid ) ) / nEq
 
-   result <- - ( nObsPerEq / 2 ) * ( nEq * ( 1 + log( 2 * pi ) ) +
-      log( det( residCov ) ) )
+   result <- 0
+   for( i in 1:nrow( resid ) ) {
+      vEq <- !is.na( resid[ i, ] )
+      if( sum( vEq ) == nEq ) {
+         result <- result - ( nEq / 2 ) * log( 2 * pi ) -
+            ( 1 / 2 ) * log( det( residCov ) ) -
+            ( 1 / 2 ) * resid[ i, , drop = FALSE ] %*% residCovInv %*%
+               t( resid[ i, , drop = FALSE ] )
+      } else if( sum( vEq ) > 0 ) {
+         nEq2 <- sum( vEq )
+         residCov2 <- residCov[ vEq, vEq, drop = FALSE ] -
+            residCov[ vEq, !vEq, drop = FALSE ] %*%
+            solve( residCov[ !vEq, !vEq, drop = FALSE ] ) %*%
+            residCov[ !vEq, vEq, drop = FALSE ]
+         residCov2Inv <- solve( residCov2 )
+         result <- result - ( nEq2 / 2 ) * log( 2 * pi ) -
+            ( 1 / 2 ) * log( det( residCov2 ) ) -
+            ( 1 / 2 ) * resid[ i, vEq, drop = FALSE ] %*%
+               residCov2Inv %*% t( resid[ i, vEq, drop = FALSE ] )
+      }
+   }
 
    if( object$method %in% c( "OLS", "2SLS" ) ){
       nSigma <- 1
