efbb0c96a15d5e6dafd67a46905b0431eadfe245
arne <arne@591db2bd-e0a0-4d22-8c19-2192dbb7da31>
2012-08-04 06:17:21 +0000

    argument "which" of model.matrix.systemfit() and model.matrix.systemfit.equation() can now be set to "xHat" in order to obtain the model matrix of the *fitted* regressors
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/systemfit@1103 591db2bd-e0a0-4d22-8c19-2192dbb7da31

--- pkg/R/model.matrix.systemfit.R
+++ pkg/R/model.matrix.systemfit.R
@@ -27,32 +27,39 @@ model.matrix.systemfit <- function( object, which = "x", ... ){
 
 ## return model matrix of a single equation
 model.matrix.systemfit.equation <- function( object, which = "x", ... ){
-   if( ! which %in% c( "x", "z" ) ) {
-      stop( "argument 'which' must be either \"x\" or \"z\"" )
-   } else if( which == "z" && is.null( object$termsInst ) ) {
-      stop( "argument 'which' can only be set to \"z\" if instruments were used" )
+   if( ! which %in% c( "x", "xHat", "z" ) ) {
+      stop( "argument 'which' must be either \"x\", \"xHat\", or \"z\"" )
+   } else if( which %in% c( "xHat", "z" ) && is.null( object$termsInst ) ) {
+      stop( "argument 'which' can only be set to \"xHat\" or \"z\" if instruments were used" )
    }
-   if( !is.null( object[[ which ]] ) ) {
-      result <- object[[ which ]]
-   } else if( !is.null( model.frame( object ) ) ) {
-      if( which == "x" ) {
-         result <- model.matrix( object$terms, data = model.frame( object ) )
-      } else {
-         result <- model.matrix( object$termsIns, data = object$modelInst )
-      }
-      attrAssign <- attributes( result )$assign
-      result <- result[ !is.na( residuals( object ) ), , drop = FALSE ]
-      attributes( result )$assign <- attrAssign
+   if( which == "xHat" ) {
+      xMat <- model.matrix( object, which = "x" )
+      zMat <- model.matrix( object, which = "z" )
+      result <- zMat %*% solve( crossprod( zMat ), crossprod( zMat, xMat ) )
    } else {
-      if( which == "x" ) {
-         stop( "returning model matrix not possible. Please re-estimate",
-            " the system with either control variable",
-            " 'x' or 'model' set to TRUE" )
+      if( !is.null( object[[ which ]] ) ) {
+         result <- object[[ which ]]
+      } else if( !is.null( model.frame( object ) ) ) {
+         if( which == "x" ) {
+            result <- model.matrix( object$terms, data = model.frame( object ) )
+         } else {
+            result <- model.matrix( object$termsIns, data = object$modelInst )
+         }
+         attrAssign <- attributes( result )$assign
+         result <- result[ !is.na( residuals( object ) ), , drop = FALSE ]
+         attributes( result )$assign <- attrAssign
       } else {
-         stop( "returning matrix of instruments not possible. Please re-estimate",
-            " the system with either control variable",
-            " 'z' or 'model' set to TRUE" )
+         if( which == "x" ) {
+            stop( "returning model matrix not possible. Please re-estimate",
+               " the system with either control variable",
+               " 'x' or 'model' set to TRUE" )
+         } else {
+            stop( "returning matrix of instruments not possible. Please re-estimate",
+               " the system with either control variable",
+               " 'z' or 'model' set to TRUE" )
+         }
       }
    }
+
    return( result )
 }
