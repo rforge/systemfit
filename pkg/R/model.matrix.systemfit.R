1000fd221285b27a6b97e027fbdc3bb1617f2c6c
arne <arne@591db2bd-e0a0-4d22-8c19-2192dbb7da31>
2012-08-04 05:24:15 +0000

    added argument "which" to model.matrix.systemfit() and model.matrix.systemfit.equation(), which can be set to "z" in order to obtain the (model) matrix of instrumental variables
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/systemfit@1100 591db2bd-e0a0-4d22-8c19-2192dbb7da31

--- pkg/R/model.matrix.systemfit.R
+++ pkg/R/model.matrix.systemfit.R
@@ -1,10 +1,10 @@
 ## return model matrix of the entire system
-model.matrix.systemfit <- function( object, ... ){
+model.matrix.systemfit <- function( object, which = "x", ... ){
    result <- matrix( NA, 0, 0 )
    mmRowNames <- NULL
    mmColNames <- NULL
    for( i in 1:length( object$eq ) ) {
-      mmi <- model.matrix( object$eq[[ i ]] ) 
+      mmi <- model.matrix( object$eq[[ i ]], which = which ) 
       result <- rbind(
          cbind( result, matrix( 0, nrow( result ), ncol( mmi ) ) ),
          cbind( matrix( 0, nrow( mmi ), ncol( result ) ), mmi ) )
@@ -26,18 +26,33 @@ model.matrix.systemfit <- function( object, ... ){
 }
 
 ## return model matrix of a single equation
-model.matrix.systemfit.equation <- function( object, ... ){
-   if( !is.null( object$x ) ) {
-      result <- object$x
+model.matrix.systemfit.equation <- function( object, which = "x", ... ){
+   if( ! which %in% c( "x", "z" ) ) {
+      stop( "argument 'which' must be either \"x\" or \"z\"" )
+   } else if( which == "z" && is.null( object$termsInst ) ) {
+      stop( "argument 'which' can only be set to \"z\" if instruments were used" )
+   }
+   if( !is.null( object[[ which ]] ) ) {
+      result <- object[[ which ]]
    } else if( !is.null( model.frame( object ) ) ) {
-      result <- model.matrix( object$terms, data = model.frame( object ) )
+      if( which == "x" ) {
+         result <- model.matrix( object$terms, data = model.frame( object ) )
+      } else {
+         result <- model.matrix( object$termsIns, data = object$modelInst )
+      }
       attrAssign <- attributes( result )$assign
       result <- result[ !is.na( residuals( object ) ), , drop = FALSE ]
       attributes( result )$assign <- attrAssign
    } else {
-      stop( "returning model matrix not possible. Please re-estimate",
-         " the system with either control variable",
-         "  'x' or 'model' set to TRUE" )
+      if( which == "x" ) {
+         stop( "returning model matrix not possible. Please re-estimate",
+            " the system with either control variable",
+            " 'x' or 'model' set to TRUE" )
+      } else {
+         stop( "returning matrix of instruments not possible. Please re-estimate",
+            " the system with either control variable",
+            " 'z' or 'model' set to TRUE" )
+      }
    }
    return( result )
 }
